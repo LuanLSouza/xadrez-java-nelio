e501c4ac4679f2ca552568411e0734b3c0aa28ef
de Souza <luan.desouza@thomsonreuters.com>
2024-10-01 10:54:46 -0300

    projeto xadrez feito

---
+++ src/boardgame/Piece.java
@@ -0,0 +1,34 @@
+package boardgame;
+
+public abstract  class Piece {
+    protected Position position;
+
+    private final Board board;
+
+    public Piece(Board board) {
+        this.board = board;
+        position = null;
+    }
+
+    protected Board getBoard() {
+        return board;
+    }
+
+    public abstract boolean[][] possibleMoves();
+
+    public boolean possibleMove(Position position){
+        return possibleMoves()[position.getRow()][position.getColumn()];
+    }
+
+    public boolean isThereAnyPossibleMove(){
+        boolean[][] mat = possibleMoves();
+        for (boolean[] booleans : mat) {
+            for (int j = 0; j < mat.length; j++) {
+                if (booleans[j]) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+}
